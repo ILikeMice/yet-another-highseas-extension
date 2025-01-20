662c3c75da6ddea62d103fb02d479a553c0d3efe
ILikeMice <ilikemice47@gmail.com>
2025-01-20 07:22:48 +0000

    kind of(?) made ability to get ship objects, maybe not the best way of doing that

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
 window.onload = () => {
     console.log("HIGHSEAS!!!!")
+    let doublooncount = Number(document.getElementsByClassName("mr-2")[0].innerText)
+    console.log(doublooncount)
+
+    console.log(document.getElementsByClassName)
+
+    let counter = 0
+    let scanning = true
+    let ships = []
+    console.log(scanning, ships, counter)
+    while (scanning  == true) {
+        console.log(counter)
+        if (document.getElementById(`shipped-ship-${counter}-shipped`) != null) {
+            ships.push(document.getElementById(`shipped-ship-${counter}-shipped`))
+            console.log(document.getElementById(`shipped-ship-${counter}-shipped`).getElementsByClassName("inline-block py-1")[1].innerHTML)
+        } else {
+            scanning = false
+        }
+        counter++
+    }
+    console.log(ships)
 }
