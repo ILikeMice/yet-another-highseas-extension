12bb9822f8c7f524ee18169cc6e7f8e57a946773
ILikeMice <ilikemice47@gmail.com>
2025-01-21 07:23:59 +0000

    turns out i can get ships way easier

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,42 @@
-window.onload = () => {
+function adddph(id) {
+    let ship = document.getElementById(id)
+    console.log(id)
+    let button = document.createElement("button")
+    button.innerText = "SHIPPPPP"
+    ship.getElementsByClassName("flex flex-wrap items-start gap-2 text-sm")[0].appendChild(button)
+}
+
+function waitForElementToExist(selector) { // "borrowed" from stackoverflow because I couldnt find another way
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
+  
+      const observer = new MutationObserver(() => {
+        if (document.querySelector(selector)) {
+          resolve(document.querySelector(selector));
+          observer.disconnect();
+        }
+      });
+  
+      observer.observe(document.body, {
+        subtree: true,
+        childList: true,
+      });
+    });
+  }
+
+window.onload = async () => {
     console.log("HIGHSEAS!!!!")
-    let doublooncount = Number(document.getElementsByClassName("mr-2")[0].innerText)
+    await new Promise((res) => setTimeout(res, 10000)); // some script updates the ships thrice at load, ugly fix but hopefully ill find something in the future
+    let doubloonspan = await waitForElementToExist(".mr-2")
+    let doublooncount = Number(doubloonspan.innerText.split(" ")[0])
     console.log(doublooncount)
 
-    console.log(document.getElementsByClassName)
-
-    let counter = 0
-    let scanning = true
-    let ships = []
-    console.log(scanning, ships, counter)
-    while (scanning  == true) {
-        console.log(counter)
-        if (document.getElementById(`shipped-ship-${counter}-shipped`) != null) {
-            ships.push(document.getElementById(`shipped-ship-${counter}-shipped`))
-            console.log(document.getElementById(`shipped-ship-${counter}-shipped`).getElementsByClassName("inline-block py-1")[1].innerHTML)
-        } else {
-            scanning = false
-        }
-        counter++
+    let shipdata = JSON.parse(window.localStorage.getItem("cache.ships"))
+    for (let i = 0; i < Object.keys(shipdata["value"]).length; i++) {
+      console.log(shipdata["value"][i])
     }
-    console.log(ships)
+    console.log(shipdata)
+
 }
