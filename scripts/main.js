271eaae936a01ca5f1f034587e0917b6367a99ea
ILikeMice <ilikemice47@gmail.com>
2025-01-23 17:32:12 +0000

    added time tab, ability to fold tabs to use less space, finished ship window stats

--- scripts/main.js
+++ scripts/main.js
@@ -64,8 +64,24 @@ function addoverview() {
   }
   let shipdiv = document.querySelector("div.space-y-4.mt-8")
   let overviewtitle = document.createElement("h2")
-  overviewtitle.className = "text-center text-2xl text-blue-500"
-  overviewtitle.innerText = "Ship Overview"
+  overviewtitle.className = "YAHSE-title text-center text-2xl text-blue-500"
+  overviewtitle.innerText = " Ship Overview"
+  let overviewbtn = document.createElement("button")
+  overviewbtn.className = "YAHSE-norotated"
+  overviewbtn.onclick = () => {
+    if (overviewbtn.getElementsByTagName("img")[0].className.includes("YAHSE-norotated")) {
+      overviewbtn.getElementsByTagName("img")[0].className = "YAHSE-rotated"
+      console.log(overviewdiv.style.display)
+      overviewdiv.style.display = "none"
+    } else {
+      overviewbtn.getElementsByTagName("img")[0].className = "YAHSE-norotated"
+      overviewdiv.style.display = "flex"
+    }
+    
+  }
+  overviewbtn.insertAdjacentHTML("afterbegin", `<img height="30" class="YAHSE-norotated" width="30" src="https://icons.hackclub.com/api/icons/hackclub-white/down-caret">`)
+
+  overviewtitle.insertAdjacentElement("afterbegin", overviewbtn)
 
   let overviewdiv = document.createElement("div")
   overviewdiv.id = "YAHSE-overview"
@@ -74,21 +90,30 @@ function addoverview() {
   let totalships = document.createElement("div")
   totalships.className = "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 transition duration-150  bg-[#9AD9EE] text-black h-10 px-4 py-2 bg-blend-color-burn"
   let ships = JSON.parse(window.localStorage.getItem("cache.ships"))
-  
+
+  let totalysws = document.createElement("div")
+  totalysws.className = "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 transition duration-150  bg-[#9AD9EE] text-black h-10 px-4 py-2 bg-blend-color-burn"
+
   let shipcount = 0
   let doublooncount = 0
   let hourcount = 0
+  let ysws = 0
   for (let i = 0; i < Object.keys(ships["value"]).length; i++) {
     doublooncount += ships["value"][i]["doubloonPayout"]
-    hourcount += ships["value"][i]["hours"]
+    
     console.log(doublooncount, ships["value"][i]["doubloonPayout"], ships["value"][i]["title"])
     if (ships["value"][i]["reshippedFromId"] == null) {
+      hourcount += ships["value"][i]["hours"]
       shipcount++
+      if (ships["value"][i]["isInYswsBase"]) {
+        ysws++
+      }
     }
   }
+
   totalships.innerText = "Total Ships: " + shipcount
   totalships.insertAdjacentHTML("afterbegin", `<img alt="doubloons" loading="lazy" height="30" width="20" decoding="async" data-nimg="1" src=https://icons.hackclub.com/api/icons/hackclub-dark/send style="color: transparent;">`)
-
+  totalysws.innerText = "Total Ships in YSWS DB: " + ysws
   
 
   let totaldoubloondiv = document.createElement("div")
@@ -104,13 +129,16 @@ function addoverview() {
 
   let dphdiv = document.createElement("div") 
   dphdiv.className = "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 transition duration-150  bg-[#9AD9EE] text-black h-10 px-4 py-2 bg-blend-color-burn"
-  dphdiv.innerHTML = "Average DPH: " + (doublooncount / hourcount).toFixed(1) + " db/h"
+  dphdiv.innerHTML = "Average DPH:   "
+  dphdiv.insertAdjacentHTML("beforeend", `<img alt="doubloons" loading="lazy" width="16" height="20" decoding="async" data-nimg="1" src="/_next/static/media/doubloon.fd63888b.svg" style="color: transparent;">`)
+  dphdiv.innerHTML += (doublooncount / hourcount).toFixed(1) + " db/h"
   
   console.log(shipdiv)
 
   overviewdiv.appendChild(totalhoursdiv)
   overviewdiv.appendChild(totaldoubloondiv)
   overviewdiv.appendChild(totalships)
+  overviewdiv.appendChild(totalysws)
   overviewdiv.appendChild(dphdiv)
   
   shipdiv.insertAdjacentElement("afterbegin", overviewdiv)
@@ -120,9 +148,156 @@ function addoverview() {
   console.log("overview")
 }
 
+function addtime() {
+  if (document.getElementById("YAHSE-time")) {
+    return
+  }
+
+  
+
+  let shipdiv = document.querySelector("div.space-y-4.mt-8")
+  let timetitle = document.createElement("h2")
+  timetitle.className = "YAHSE-title text-center text-2xl text-blue-500"
+  timetitle.innerText = " Time"
+  let titlebtn = document.createElement("button")
+  titlebtn.className = "YAHSE-norotated"
+  titlebtn.onclick = () => {
+    if (titlebtn.getElementsByTagName("img")[0].className.includes("YAHSE-norotated")) {
+      titlebtn.getElementsByTagName("img")[0].className = "YAHSE-rotated"
+      
+      timediv.style.display = "none"
+    } else {
+      titlebtn.getElementsByTagName("img")[0].className = "YAHSE-norotated"
+      timediv.style.display = "flex"
+    }
+    
+  }
+
+  titlebtn.insertAdjacentHTML("afterbegin", `<img height="30" class="YAHSE-norotated" width="30" src="https://icons.hackclub.com/api/icons/hackclub-white/down-caret">`)
+
+  timetitle.insertAdjacentElement("afterbegin", titlebtn)
+
+  let timebar = document.createElement("div")
+  timebar.className = "YAHSE-timebar"
+
+  let timediv = document.createElement("div")
+  timediv.id = "YAHSE-time"
+  timediv.className = "YAHSE-timediv rounded-lg bg-card text-card-foreground shadow-sm bg-blend-color-burn flex flex-col sm:gap-2 sm:items-center p-4  transition-colors duration-200"
+
+  let goaldiv = document.createElement("div")
+  goaldiv.className = "YAHSE-goaldiv inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 transition duration-150  bg-[#9AD9EE] text-black h-10 px-4 py-2 bg-blend-color-burn"
+  
+  let itemdatalist = document.createElement("datalist")
+  itemdatalist.id = "YAHSE-itemdatalist"
+
+  let itemdata = JSON.parse(window.localStorage.getItem("cache.shopItems"))["value"]
+  let itemlist = []
+  for (let i = 0; i < Object.keys(itemdata).length; i++) {
+    let option = document.createElement("option")
+    option.value = itemdata[i]["id"].replaceAll("item_", "")
+    itemlist.push(itemdata[i]["id"].replaceAll("item_", ""))
+    itemdatalist.appendChild(option)
+  }
+
+  let iteminput = document.createElement("input")
+  iteminput.className = "YAHSE-iteminput"
+  iteminput.id = "YAHSE-iteminput"
+  iteminput.setAttribute("list", "YAHSE-itemdatalist")
+
+  iteminput.oninput = () => setgoal(itemlist)
+
+  function setgoal(items) {
+    if (!items.includes(document.getElementById("YAHSE-iteminput").value)) {
+      return
+    }
+    console.log(items, document.getElementById("YAHSE-iteminput").value)
+    let itemdata = JSON.parse(window.localStorage.getItem("cache.shopItems"))["value"]
+    let shipdata = JSON.parse(window.localStorage.getItem("cache.ships"))["value"]
+    let itemname, itemprice, itempic, itemsubtitle
+    for (let i = 0; i < Object.keys(itemdata).length; i++) {
+      if (itemdata[i]["id"] == "item_" + document.getElementById("YAHSE-iteminput").value) {
+        itemname = itemdata[i]["name"]
+        itemprice = itemdata[i]["priceGlobal"]
+        itempic = itemdata[i]["imageUrl"]
+        itemsubtitle = itemdata[i]["subtitle"]
+      }
+    }
+    let hourcount = 0
+    let doublooncount = 0
+    for (let i = 0; i < Object.keys(shipdata).length; i++) {
+      doublooncount += shipdata[i]["doubloonPayout"]
+  
+      if (shipdata[i]["reshippedFromId"] == null) {
+        hourcount += shipdata[i]["hours"]
+      }
+    }
+    console.log((doublooncount/hourcount))
+    let itemdisplay = document.getElementById("YAHSE-itemdisplaydiv")
+    
+    let itemobject = `<div style="transform: none">
+        <div class="rounded-lg bg-card text-card-foreground bg-blend-color-burn h-full flex flex-col overflow-hidden shadow-lg transition-shadow duration-300 hover:shadow-xl"
+            style="
+                background-size: 10rem 100%;
+                background-repeat: repeat-x;
+                background-color: rgba(255, 255, 255, 0.94);
+            ">
+            <div class="flex flex-col space-y-1.5 p-6 pb-2">
+                <div class="justify-between inline-block items-start">
+                    <h3 class="tracking-tight text-xl font-bold text-center">
+                        ${itemname}
+                    </h3>
+                </div>
+                <hr />
+                <p class="text-sm text-gray-600 mt-1">${itemsubtitle}</p>
+                <p class="inline-flex gap-3 items-center">
+                    <span class="text-green-500 font-semibold flex items-center">
+                        <img src="doubloon.svg" alt="doubloons" width="20" height="20" class="mr-1" />${itemprice}
+                    </span>
+                    <span class="text-xs text-gray-600">(approx. ${(itemprice/(doublooncount/hourcount)).toFixed(true)} hours) (${(itemprice/(doublooncount/hourcount) - hourcount).toFixed(true)}h to go!)</span>
+                </p>
+            </div>
+            <div class="p-0 flex-grow">
+                <div class="h-48 overflow-hidden" style="margin-bottom: 50px">
+                    <img src="${itempic}"
+                        alt='Framework Laptop 13"'
+                        class="w-full h-full object-cover transition-transform duration-300 hover:scale-105" />
+                </div>
+            </div>
+        </div>
+    </div>`
+  
+
+    itemdisplay.innerHTML = ""
+    itemdisplay.insertAdjacentHTML("beforeend", itemobject)
+    console.log("added")
+  }
+
+  let itemdisplaydiv = document.createElement("div")
+  itemdisplaydiv.id = "YAHSE-itemdisplaydiv"
+  itemdisplaydiv.className = "YAHSE-itemdisplaydiv"
+
+  
+  goaldiv.innerText = "Goal: "
+  goaldiv.appendChild(iteminput)
+  goaldiv.appendChild(itemdatalist)
+
+  timebar.appendChild(goaldiv)
+  
+  
+  timediv.appendChild(timebar)
+  timediv.appendChild(itemdisplaydiv)
+
+  shipdiv.insertAdjacentElement("afterbegin", timediv)
+  shipdiv.insertAdjacentElement("afterbegin", timetitle)
+  
+}
+
+
+
 function addwindowstats() {
   if (!document.getElementById("YAHSE-windowdph")) {
-    let shipdata = window.localStorage.getItem("cache.ships")
+
+    let shipdata = JSON.parse(window.localStorage.getItem("cache.ships"))
     let shiptitle = document.querySelector("h2.text-3xl.font-bold").innerText
     let pillcontainer = document.querySelector("div.flex.items-center.gap-4.mt-4")
     console.log(shiptitle)
@@ -130,7 +305,7 @@ function addwindowstats() {
     let shipdoubloons = 0
     let shiphours = 0
 
-    for (let i = 0; i < shipdata["value"].length; i++) {
+    for (let i = 0; i < Object.keys(shipdata["value"]).length; i++) {
       if (shipdata["value"][i]["title"] == shiptitle) {
         shipdoubloons += shipdata["value"][i]["doubloonPayout"]
         shiphours += shipdata["value"][i]["hours"]
@@ -167,13 +342,16 @@ function checkitems() {
     addwindowstats()
   }
   
-  addoverview()
-  let shipdata = JSON.parse(window.localStorage.getItem("cache.ships"))
-  for (let i = 0; i < Object.keys(shipdata["value"]).length; i++) {
-    adddph(`shipped-ship-${i}-shipped`, shipdata["value"])
-    
-    
+  if (window.location.pathname == "/shipyard") { 
+    addoverview()
+    addtime()
+
+    let shipdata = JSON.parse(window.localStorage.getItem("cache.ships"))
+    for (let i = 0; i < Object.keys(shipdata["value"]).length; i++) {
+      adddph(`shipped-ship-${i}-shipped`, shipdata["value"])
+    }
   }
+  
 }
 
 window.onload = () => {
